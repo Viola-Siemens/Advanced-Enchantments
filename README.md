a39ed52e25e7eb62086ca251da7257ec444480f7
刘冬煜 <10185101162@stu.ecnu.edu.cn>
2023-11-16 13:29:53 +0800

    部分说明与logo。联动附魔描述

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ This mod adds advanced versions for some of the vanilla enchantments.
 - Flame II: makes entities burns longer time after hit by arrows.
 - Infinity II: also makes spectral arrows and tipped arrows infinite.
 
+And it also adds an enchantment:
+
+- Damp (for chestplates and elytras): you can use tridents with riptide enchantment, take the conduit power effect from conduit in any case, even not in water.
+
--- src/main/java/com/hexagram2021/advanced_enchantments/common/enchantments/DampEnchantment.java
+++ src/main/java/com/hexagram2021/advanced_enchantments/common/enchantments/DampEnchantment.java
@@ -1,6 +1,9 @@
 package com.hexagram2021.advanced_enchantments.common.enchantments;
 
 import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.item.AxeItem;
+import net.minecraft.world.item.ElytraItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentCategory;
 
@@ -21,5 +24,10 @@ public class DampEnchantment extends Enchantment {
 	@Override
 	public int getMaxCost(int level) {
 		return level * 30 + 20;
+	}
+
+	@Override
+	public boolean canEnchant(ItemStack itemStack) {
+		return itemStack.getItem() instanceof ElytraItem || super.canEnchant(itemStack);
 	}
 }
